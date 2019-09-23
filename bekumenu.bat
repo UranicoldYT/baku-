fa5bda833b5dfdc2a46fa58ceb1832ddba2aa60f
UranicoldYT <54318227+UranicoldYT@users.noreply.github.com>
2019-09-23 13:23:11 +0200

    created beku menu

---
+++ bekumenu.bat
@@ -0,0 +1,13 @@
+@echo off
+cls
+echo  Welcome to the Beku Menu
+TIMEOUT 3 >nul
+echo Searching for game.
+TIMEOUT 1 >nul
+echo Searching for game..
+TIMEOUT 1 >nul
+echo Searching for game...
+TIMEOUT 1 >nul
+echo Searching for game....
+TIMEOUT 1 >nul
+echo This game is too outdated
